c54504673d135942a92279769e83b93041c470a0
christianonoh <onohchibyk@gmail.com>
2023-03-28 23:31:39 +0100

    Add html, css loader and local dev server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
+import './style.css';
+
 const component = () => {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'usom'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
