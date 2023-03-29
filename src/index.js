67d38f3797e0285014158460c084b3f1bf32af5a
christianonoh <onohchibyk@gmail.com>
2023-03-29 04:14:16 +0100

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
+/* eslint-disable no-unused-vars */
 import _ from 'lodash';
 import './style.css';
 
-const component = () => {
-  const element = document.createElement('div');
+const tasksArray = [
+  {
+    description: 'Learn JavaScript',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Buy groceries',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Go for a run',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'usom'], ' ');
-  element.classList.add('hello');
+const taskList = document.getElementById('tasks');
+tasksArray.forEach((task) => {
+  taskList.innerHTML += `
+  <li class="task-item">
+  <span class="task-item-content">
+    <input type="checkbox">
+    <span class="description">${task.description}</span>
+  </span>
+  <span class="move-task">
+  <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+  </span>
+</li>`;
+});
 
-  return element;
-};
+// class Tasks{
+//   constructor() {
+//     this.taskList = document.getElementById('tasks');
+//     this.submitButton = document.getElementById('submitButton');
+//     this.
+//   }
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
